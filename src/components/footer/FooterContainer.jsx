ca9cff47fa98189fa5165ff4788539d5cb2937fb
SumonReza8525 <sumon.esd.bsmrstu@gmail.com>
2026-03-01 11:55:41 +0600

    all the design done

--- src/components/footer/FooterContainer.jsx
+++ src/components/footer/FooterContainer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import footerImg from "../../assets/bg-shadow.png";
 const FooterContainer = () => {
   return (
-    <div className="w-full border-2 mt-50 relative">
-      <div className="p-2 border-2 border-white absolute top-[-25%] rounded-2xl w-[80%] min-h-50 mx-auto z-40 left-[10%] bg-[#ffffff3f]">
+    <div className="w-full   mt-70 relative">
+      <div className="p-2 border-2 border-white absolute top-[-20%] rounded-2xl w-[80%] min-h-50 mx-auto z-40 left-[10%] bg-[#ffffff3f]">
         <div
           className=" rounded-2xl flex flex-col items-center justify-center bg-white space-y-2 p-1.5 min-h-50 shadow-accent-content"
           style={{
@@ -34,7 +34,48 @@ const FooterContainer = () => {
           </div>
         </div>
       </div>
-      <div className="bg-black w-full min-h-96">footer2</div>
+      <div className="bg-black w-full min-h-96 py-8 text-white">
+        <div className="w-[80%]  mx-auto mt-32  md:mt-40 space-y-4 grid grid-cols-1 md:grid-cols-3 gap-8 p-2 ">
+          <div>
+            <h3 className="text-xl font-bold mb-2">About Us</h3>
+            <p className="text-gray-400">
+              We are a passionate team <br />
+              dedicated to providing the best <br /> services to our customers
+            </p>
+          </div>
+          <div>
+            <h3 className="text-xl  mb-2 font-bold">Quick Links</h3>
+            <ul className="list-disc list-inside text-gray-400">
+              <li>Home</li>
+              <li>Services</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="flex flex-col justify-center items-start space-y-3">
+            <h3 className="text-xl font-bold">Subscribe</h3>
+            <p className="text-gray-400">
+              Subscribe to our newsletter for the latest updates
+            </p>
+            <div className="flex gap-3">
+              {" "}
+              <input
+                placeholder="Enter your email"
+                className="border-2 px-3 py-1.5 rounded-xl bg-white border-gray-300"
+                type="text"
+                name=""
+                id=""
+              />
+              <button className="bg-linear-to-r from-pink-400 to-yellow-400 text-black font-bold py-2 px-6 rounded-xl shadow-lg transition-transform transform hover:scale-105">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+        <p className="text-center mt-10 text-gray-400">
+          @2024 Your Company All Rights Reserved
+        </p>
+      </div>
     </div>
   );
 };
